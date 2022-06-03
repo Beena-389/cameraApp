abd586d70d758405a2ba55a450e663d548b28fe5
Beena Devi <you@example.com>
2022-06-03 14:22:00 +0530

    initial commit

--- test4/app.py
+++ test4/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, Response, request
 import cv2
 import datetime, time
-import os, sys
+
 import numpy as np
-from threading import Thread
+
 
 
 global capture,rec_frame, grey, switch, neg, face, rec, out 
@@ -14,11 +14,7 @@ face=0
 switch=1
 rec=0
 
-#make shots directory to save pics
-try:
-    os.mkdir('./shots')
-except OSError as error:
-    pass
+
 
 #Load pretrained face detection model    
 net = cv2.dnn.readNetFromCaffe('./deploy.prototxt.txt', './res10_300x300_ssd_iter_140000.caffemodel')
@@ -72,25 +68,12 @@ def gen_frames():  # generate frame by frame from camera
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             if(neg):
                 frame=cv2.bitwise_not(frame)    
-            if(capture):
-                capture=0
-                now = datetime.datetime.now()
-                p = os.path.sep.join(['shots', "shot_{}.png".format(str(now).replace(":",''))])
-                cv2.imwrite(p, frame)
             
-            if(rec):
-                rec_frame=frame
-                frame= cv2.putText(cv2.flip(frame,1),"Recording...", (0,25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),4)
-                frame=cv2.flip(frame,1)
+            
+            
             
                 
-            try:
-                ret, buffer = cv2.imencode('.jpg', cv2.flip(frame,1))
-                frame = buffer.tobytes()
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-            except Exception as e:
-                pass
+            
                 
         else:
             pass
@@ -109,10 +92,8 @@ def video_feed():
 def tasks():
     global switch,camera
     if request.method == 'POST':
-        if request.form.get('click') == 'Capture':
-            global capture
-            capture=1
-        elif  request.form.get('grey') == 'Grey':
+        
+        if  request.form.get('grey') == 'Grey':
             global grey
             grey=not grey
         elif  request.form.get('neg') == 'Negative':
@@ -133,19 +114,7 @@ def tasks():
             else:
                 camera = cv2.VideoCapture(0)
                 switch=1
-        elif  request.form.get('rec') == 'Start/Stop Recording':
-            global rec, out
-            rec= not rec
-            if(rec):
-                now=datetime.datetime.now() 
-                fourcc = cv2.VideoWriter_fourcc(*'XVID')
-                out = cv2.VideoWriter('vid_{}.avi'.format(str(now).replace(":",'')), fourcc, 20.0, (640, 480))
-                #Start new thread for recording the video
-                thread = Thread(target = record, args=[out,])
-                thread.start()
-            elif(rec==False):
-                out.release()
-                          
+        
                  
     elif request.method=='GET':
         return render_template('index.html')
